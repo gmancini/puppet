ae489776dedc6d9cea5f0bc38d10283c47f34230
Genaro Mancini <genaro.mancini@gmail.com>
2019-12-12 21:49:26 +0000

    Removing duplicate

--- manifests/1puppet.pp
+++
@@ -1,4 +0,0 @@
-file {'/tmp/hello.txt':
-  ensure => file,
-  content => "############Archivo mantenido por puppet####################\nTest de un archivo nuevo\n#####################################fin####################################\n",
-}
